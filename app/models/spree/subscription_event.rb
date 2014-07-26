3387d02d711ec15cb8e74b0739002082f1586fb7
Denis Ivanov <visible@jumph4x.net>
2014-07-26 00:13:18 -0700

    Workable JSON deserialization

--- app/models/spree/subscription_event.rb
+++ app/models/spree/subscription_event.rb
@@ -1,11 +1,15 @@
 module Spree
   class SubscriptionEvent < ActiveRecord::Base
-    serialize :response
-    
+    serialize :response, JSON
+
     belongs_to :subscription
     validates :event_id, :subscription_id, presence: true
     validates :event_id, uniqueness: true
 
     attr_readonly :event_id, :subscription_id, :request_type
+
+    def parsed_response
+      JSON.parse response
+    end
   end
 end
