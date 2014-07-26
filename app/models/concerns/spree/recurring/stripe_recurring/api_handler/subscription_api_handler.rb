cceb52896db98727d7a8d1e9a750fa43a5c4733a
Denis Ivanov <visible@jumph4x.net>
2014-07-26 00:13:37 -0700

    attr_accessor for Stripe API parameters

--- app/models/concerns/spree/recurring/stripe_recurring/api_handler/subscription_api_handler.rb
+++ app/models/concerns/spree/recurring/stripe_recurring/api_handler/subscription_api_handler.rb
@@ -6,17 +6,25 @@ module Spree
           def subscribe subscription
             raise_invalid_object_error(subscription, Spree::Subscription)
             customer = subscription.user.find_or_create_stripe_customer(subscription.card_token)
-            customer.subscriptions.create(plan: subscription.api_plan_id, card: subscription.card_token)
+            customer.subscriptions.create(extra_opts(subscription).merge(plan: subscription.api_plan_id, card: subscription.card_token))
           end
 
           def change_plan subscription
             raise_invalid_object_error(subscription, Spree::Subscription)
-            subscription.user.api_customer.update_subscription(plan: subscription.api_plan_id, card: subscription.card_token)
+            subscription.user.api_customer.update_subscription(extra_opts(subscription).merge(plan: subscription.api_plan_id, card: subscription.card_token))
           end
 
           def unsubscribe subscription
             raise_invalid_object_error(subscription, Spree::Subscription)
-            subscription.user.api_customer.cancel_subscription(at_period_end: true)
+            subscription.user.api_customer.cancel_subscription(cancelation_opts)
+          end
+
+          def extra_opts subscription
+            subscription.stripe_opts.presence || {}
+          end
+
+          def cancelation_opts
+            {at_period_end: true}
           end
         end
       end
